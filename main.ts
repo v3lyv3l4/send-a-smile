beeae5ad83e587a485744420fcd2e34b75286391
v3lyv3l4 <112582718+v3lyv3l4@users.noreply.github.com>
2022-10-27 18:10:16 -0700

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    radio.sendString("smile")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+radio.setGroup(1)
